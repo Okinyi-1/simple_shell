c3d5c76f1b04167b128bcc1ddc7679adaf474c29
Paul Kirugu <111176482+pkirugu89@users.noreply.github.com>
2023-07-27 11:09:17 +0300

    Update exec.c

--- exec.c
+++ exec.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * execute_command - performs requested command
  *
@@ -7,35 +6,30 @@
  * @args: pointer to arguments eneterd
  * @env: pointer to the simple shell environment
  */
-
 void execute_command(char *command, char **args, char **env)
 {
 	char *cmd_path;
 	pid_t pid;
-	int status, exit_status;
-
+	int status;
 	/* Check if the command is a built-in command */
 	if (strcmp(command, "printenv") == 0)
 	{
 		print_environment();
 		return;
-	}
-	/* Check if the command is a full path to an executable*/
+	} /* Check if the command is a full path to an executable*/
 	if (access(command, X_OK) == 0)
 	{
 		cmd_path = strdup(command);
 	}
 	else
-	{
-		/* If not, try to find the command in the directories listed in the PATH */
+	{ /* If not, try to find the command in the directories listed in the PATH */
 		cmd_path = path_lookup(command);
 		if (cmd_path == NULL)
 		{
 			printf("Command not found: %s\n", command);
 			return;
 		}
-	}
-	/* Fork a new process to execute the command */
+	} /* Fork a new process to execute the command */
 	pid = fork();
 	if (pid < 0)
 	{
@@ -44,30 +38,14 @@ void execute_command(char *command, char **args, char **env)
 	}
 	else if (pid == 0)
 	{
-		/* Child process: execute the command with arguments */
 		execve(cmd_path, args, env);
-		/* If execution reaches here, there was an error with execve */
 		perror("Command execution error");
 		exit(EXIT_FAILURE);
 	}
-	else
+	wait(&status);
+	if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
 	{
-		/* Parent process: wait for the child to finish */
-		wait(&status);
-
-		if (WIFEXITED(status))
-		{
-			exit_status = WEXITSTATUS(status);
-			if (exit_status != 0)
-				/* Non-zero exit status indicates an error */
-				printf("Command returned with an error (exit status: %d)\n", exit_status);
-		}
-		else if (WIFSIGNALED(status))
-		{
-			/* If the process was terminated by a signal */
-			printf("Command terminated by signal\n");
-		}
-		/* Free the memory allocated for the command path */
-		free(cmd_path);
+		printf("Command returned with an error (exit status: %d)\n", exit_status);
 	}
+	free(cmd_path);
 }
