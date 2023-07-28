a0707619f52ef90564fac196a4e5b40d3667df42
Okinyi-1 <okinyio.tonnie@gmail.com>
2023-07-29 00:43:28 +0300

    updated exec.c

--- exec.c
+++ exec.c
@@ -11,29 +11,24 @@ void execute_command(char *command, char **args, char **env)
 	char *cmd_path;
 	pid_t pid;
 	int status;
-	char exit_status_str[10];
+	/*char exit_status_str[10];*/
 
-	/* Check if the command is a built-in command */
 	if (strcmp(command, "printenv") == 0)
 	{
 		print_environment();
 		return;
-	} /* Check if the command is a full path to an executable*/
-	if (access(command, X_OK) == 0)
-	{
-		cmd_path = strdup(command);
 	}
+	if (access(command, X_OK) == 0)
+		cmd_path = strdup(command);
 	else
-	{ /* If not, try to find the command in the directories listed in the PATH */
+	{
 		cmd_path = path_lookup(command);
 		if (cmd_path == NULL)
 		{
 			write(STDOUT_FILENO, "command not found: ", 19);
-			write(STDOUT_FILENO, command, strlen(command));
-			write(STDOUT_FILENO, "\n", 1);
 			return;
 		}
-	} /* Fork a new process to execute the command */
+	}
 	pid = fork();
 	if (pid < 0)
 	{
@@ -50,9 +45,6 @@ void execute_command(char *command, char **args, char **env)
 	if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
 	{
 		write(STDOUT_FILENO, "Command returned with an error (exit status; ", 45);
-		sprintf(exit_status_str," %d", WEXITSTATUS(status));
-		write(STDOUT_FILENO, exit_status_str, strlen(exit_status_str));
-		write(STDOUT_FILENO, ")\n", 2);
 	}
 	free(cmd_path);
 }
